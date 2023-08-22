7263a58662adc80113f4c04e66afd00b2f9ced68
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-08-21 19:55:58 -0500

    [UPDATE] summary from the Flatten

--- UWP Toolkit/Extensions/EnumerableExtensions.cs
+++ UWP Toolkit/Extensions/EnumerableExtensions.cs
@@ -19,7 +19,7 @@ public static class EnumerableExtensions
     //    return result;
     //}
     /// <summary>
-    /// Flattens a list of objects
+    /// Flattens a collection of nested objects, returning a flat sequence of objects.
     /// </summary>
     /// <param name="collection"></param>
     /// <returns></returns>
@@ -38,41 +38,6 @@ public static class EnumerableExtensions
         }
         return result;
     }
-    #region Flat Example
-    /*
-     * List<object> nestedList = new()
-{
-    1,
-    new List<object> { 2, 3, new List<object> { 4, 5 } },
-    6,
-    new List<object> { 7 },
-    new List<object> { 8, 9, new List<object> { 10, 11, new List<object> { 12,13,14,15} } },
-    16,
-    new List<object> { 17, 18, new List<object> { 19, 20, new List<object> { 21,22,23,24,25,26,27,28,29,30,31,32,33,34,35 } } },
-    36,
-};
-
-List<object> stringList = new()
-{
-    "Hello",
-    new List<object> { "World", "!" },
-    "How",
-    new List<object> { "are", "you", new List<object> { "doing", "today", new List<object> { "my", "friend", "!" } } },
-    "I",
-    new List<object> { "am", "fine", new List<object> { "thank", "you", new List<object> { "for", "asking", "!" } } },
-    "Goodbye",
-};
-
-
-List<object> newList3 = nestedList.Flat().ToList();
-foreach (var item in newList3)
-    Console.WriteLine(item);
-
-List<object> newListString = stringList.Flat().ToList();
-foreach (var item in newListString)
-    Console.WriteLine(item);
-     */
-    #endregion Flat Example
 
     /// <summary>
     /// Converts an <see cref="IEnumerable{T}"/> to an <see cref="ObservableCollection{T}"/>
@@ -86,12 +51,4 @@ foreach (var item in newListString)
         if (source is null) throw new ArgumentNullException(nameof(source));
         return new ObservableCollection<T>(source);
     }
-
-    #region ToObservableCollection Example
-    /*
-     * ObservableCollection<object> newListString = stringList.Flat().ToObservableCollection();
-foreach (var item in newListString)
-    Console.WriteLine(item);
-     */
-    #endregion ToObservableCollection Example
 }
