e426ace32a3c5e4cddea832a8baa688c8e2cabf9
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-08-21 20:56:48 -0500

    [DELETE] All of examples

--- UWP Toolkit/Helpers/Json.cs
+++ UWP Toolkit/Helpers/Json.cs
@@ -14,13 +14,6 @@ public static class Json
     /// Returns an object of type T.
     /// </returns>
     public static async Task<T> ToObjectAsync<T>(string value) => await Task.Run<T>(() => JsonConvert.DeserializeObject<T>(value));
-    #region ToObjectAsync Examples
-    // Example 1:
-    // var json = @"{""Name"":""Alex Falcon"",""Occupation"": ""Software Developer""}";
-    // var obj = await Json.ToObjectAsync<Dictionary<string, string>>(json);
-    // Output: obj["Name"] = "Alex Falcon"
-    // Output: obj["Occupation"] = "Software Developer"
-    #endregion ToObjectAsync Examples
 
     /// <summary>
     /// Converts a specified object to a JSON string.
@@ -28,10 +21,4 @@ public static class Json
     /// <param name="value"></param>
     /// <returns></returns>
     public static async Task<string> StringifyAsync(object value) => await Task.Run<string>(() => JsonConvert.SerializeObject(value));
-    #region StringifyAsync Examples
-    // Example 1:
-    // var obj = new { Name = "Alex Falcon", Occupation = "Software Developer" };
-    // var json = await Json.StringifyAsync(obj);
-    // Output: {"Name":"Alex Falcon","Occupation": "Software Developer"}
-    #endregion StringifyAsync Examples
 }
