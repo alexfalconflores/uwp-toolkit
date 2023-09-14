c30acaa33b79aee9bfc03289c1cfec76a0e9b8c2
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-09-13 19:57:22 -0500

    [ADD] Documentation

--- UWP Toolkit/Converters/ElementThemeToIconConverter.cs
+++ UWP Toolkit/Converters/ElementThemeToIconConverter.cs
@@ -4,8 +4,12 @@ using Windows.UI.Xaml.Data;
 
 namespace UWP_Toolkit.Converters;
 
+/// <summary>
+/// Convert an ElementTheme to a Symbol Icon. Default is a TVMonitor 'uE7F4', Light is a Sun 'uE706', Dark is a Moon 'uE708'
+/// </summary>
 public class ElementThemeToIconConverter : IValueConverter
 {
+    /// <inheritdoc/>
     public object Convert(object value, Type targetType, object parameter, string language) =>
             value switch
             {
@@ -14,8 +18,7 @@ public class ElementThemeToIconConverter : IValueConverter
                 ElementTheme.Dark => "\uE708",
                 _ => null,
             };
-
-
+    /// <inheritdoc/>
     public object ConvertBack(object value, Type targetType, object parameter, string language) =>
         throw new NotImplementedException();
 }
