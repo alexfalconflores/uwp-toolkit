094273428550c7436fd6f5a7caf4b6d4f8820b25
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-09-13 20:19:37 -0500

    [ADD] Double and Float

---
+++ UWP Toolkit/Extensions/DoubleExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace UWP_Toolkit.Extensions;
+
+public static class DoubleExtensions
+{
+    /// <summary>
+    /// Check if the number is NaN.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static bool IsNaN(this double number) => double.IsNaN(number);
+}
---
+++ UWP Toolkit/Extensions/FloatExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace UWP_Toolkit.Extensions;
+
+public static class FloatExtensions
+{
+    //public static float Clamp(this float value, float min, float max)
+    //{
+    //    if (value < min)
+    //        return min;
+    //    if (value > max)
+    //        return max;
+    //    return value;
+    //}
+
+    /// <summary>
+    /// Check if the number is NaN.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static bool IsNaN(this float number) => float.IsNaN(number);
+}
