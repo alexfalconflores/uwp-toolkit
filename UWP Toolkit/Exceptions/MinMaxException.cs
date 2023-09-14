230e6496498183906078a8c6fb26239cb33ed43d
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-09-13 20:01:05 -0500

    [ADD] MinMax and MultipleOfZero

---
+++ UWP Toolkit/Exceptions/MinMaxException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UWP_Toolkit.Exceptions;
+
+/// <summary>
+/// The exception that is thrown when a value is outside the range of valid values.
+/// </summary>
+[Serializable]
+public class MinMaxException : ArithmeticException
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MinMaxException"/> class.
+    /// </summary>
+    public MinMaxException() : base()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MinMaxException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message"></param>
+    public MinMaxException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MinMaxException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public MinMaxException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MinMaxException"/> class with serialized data.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
+    protected MinMaxException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ UWP Toolkit/Exceptions/MultipleOfZeroException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UWP_Toolkit.Exceptions;
+
+/// <summary>
+/// The exception that is thrown when an attempt is made to divide an integer or BigInteger value by zero.
+/// </summary>
+[Serializable]
+public class MultipleOfZeroException : ArithmeticException
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultipleOfZeroException"/> class
+    /// </summary>
+    public MultipleOfZeroException() : base()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultipleOfZeroException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message"></param>
+    public MultipleOfZeroException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultipleOfZeroException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public MultipleOfZeroException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MultipleOfZeroException"/> class with serialized data.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
+    protected MultipleOfZeroException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
