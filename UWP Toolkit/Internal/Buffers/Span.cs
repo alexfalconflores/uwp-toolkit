78b8b340695f6c57d4ada07fb0ca4b98d9252b9f
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-12-03 20:08:57 -0500

    [ADD] SpanAction Port

---
+++ UWP Toolkit/Internal/Buffers/Span.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Toolkit.Internal.Buffers;
+
+/// <summary>
+///  Encapsulates a method that receives a span of objects of type T and a state object
+///  of type TArg.
+/// </summary>
+/// <typeparam name="T">The type of the objects in the span.</typeparam>
+/// <typeparam name="TArg">The type of the object that represents the state.</typeparam>
+/// <param name="span">A span of objects of type T.</param>
+/// <param name="arg">A state object of type TArg.</param>
+public delegate void SpanAction<T, in TArg>(Span<T> span, TArg arg);
