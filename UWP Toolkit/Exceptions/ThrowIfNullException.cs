84c521340cb6c133f7917395f66745e003e719ba
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-12-03 20:22:03 -0500

    [ADD] Throw if null exception

---
+++ UWP Toolkit/Exceptions/ThrowIfNullException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UWP_Toolkit.Exceptions;
+
+/// <summary>
+/// The ArgumentException is thrown when an argument is null when it shouldn't be.
+/// Ported from .NET 7.0 source code.
+/// </summary>
+[Serializable]
+public class ThrowIfNullException : ArgumentNullException
+{
+    /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.
+    /// Ported from .NET 7.0 source code.</summary>
+    /// <param name="argument">The reference type argument to validate as non-null.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    public static void ThrowIfNull([NotNull] object? argument, [System.Runtime.CompilerServices.CallerArgumentExpression("argument")] string? paramName = null)
+    {
+        if (argument is null)
+        {
+            Throw(paramName);
+        }
+    }
+
+    [DoesNotReturn]
+    internal static void Throw(string? paramName) =>
+            throw new ArgumentNullException(paramName);
+}
